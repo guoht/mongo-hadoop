62d5477a071e52a334c80d3feed5ca4089a1c005
guohaitao02 <guohaitao02@58.com>
2023-04-07 11:16:42 +0800

    1.maven环境配置 2.优化连接池参数，减少链接

--- core/src/main/java/com/mongodb/hadoop/util/MongoConfigUtil.java
+++ core/src/main/java/com/mongodb/hadoop/util/MongoConfigUtil.java
@@ -17,13 +17,16 @@
 
 package com.mongodb.hadoop.util;
 
-import com.mongodb.BasicDBObject;
-import com.mongodb.DBCollection;
-import com.mongodb.DBObject;
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
 import com.mongodb.MongoClientURI;
 import com.mongodb.MongoURI;
+import com.mongodb.ReadConcern;
+import com.mongodb.ReadPreference;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+import com.mongodb.BasicDBObject;
 import com.mongodb.hadoop.splitter.MongoSplitter;
 import com.mongodb.hadoop.splitter.SampleSplitter;
 import com.mongodb.util.JSON;
@@ -1114,7 +1117,27 @@ public final class MongoConfigUtil {
     private static MongoClient getMongoClient(final MongoClientURI uri) throws UnknownHostException {
         MongoClient mongoClient = CLIENTS.get().get(uri);
             if (mongoClient == null) {
-                mongoClient = new MongoClient(uri);
+                MongoClientOptions.Builder builder = new MongoClientOptions.Builder();
+                //与目标数据库能够建立的最大connection数量为50
+                builder.connectionsPerHost(5);
+                builder.maxConnectionIdleTime(30000);
+                builder.maxConnectionLifeTime(360000);
+                builder.socketKeepAlive(true);
+                //如果当前所有的connection都在使用中，则每个connection上可以有50个线程排队等待
+                builder.threadsAllowedToBlockForConnectionMultiplier(5);
+                /*
+                 * 一个线程访问数据库的时候，在成功获取到一个可用数据库连接之前的最长等待时间为2分钟
+                 * 这里比较危险，如果超过maxWaitTime都没有获取到这个连接的话，该线程就会抛出Exception
+                 * 故这里设置的maxWaitTime应该足够大，以免由于排队线程过多造成的数据库访问失败
+                 */
+                builder.maxWaitTime(1000 * 60 * 2);
+                //与数据库建立连接的timeout设置为1分钟
+                builder.connectTimeout(1000 * 60 * 1);
+                builder.readPreference(ReadPreference.nearest());
+                builder.readConcern(ReadConcern.LOCAL);
+
+                MongoClientURI localURI = new MongoClientURI(uri.getURI() , builder);
+                mongoClient = new MongoClient(localURI);
                 CLIENTS.get().put(uri, mongoClient);
                 URI_MAP.get().put(mongoClient, uri);
             }
